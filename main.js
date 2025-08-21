587e6acbffc38037c158b2d723a8db2c333e9c31
Thomas <thomas@targetmultimidia.com.br>
2025-08-21 17:03:29 -0300

    car model imported

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from './libs/three.module.js';
 import { VRButton } from './libs/VRButton.js';
+import { GLTFLoader } from '../libs/GLTFLoader.js';
+import { DRACOLoader } from '../libs/DRACOLoader.js';
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x202020);
@@ -12,9 +14,17 @@ renderer.xr.enabled = true;
 document.body.appendChild(renderer.domElement);
 document.body.appendChild(VRButton.createButton(renderer));
 
-const light = new THREE.HemisphereLight(0xffffff, 0x444444);
-light.position.set(0, 20, 0);
-scene.add(light);
+// const light = new THREE.HemisphereLight(0xffffff, 0x444444);
+// light.position.set(0, 20, 0);
+// scene.add(light);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, .8);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+const pointLight = new THREE.PointLight(0xffffff, 2, 100);
+pointLight.position.set(0, 20, 0);
+scene.add(ambientLight);
+scene.add(directionalLight);
+scene.add(pointLight);
 
 const floor = new THREE.Mesh(
     new THREE.PlaneGeometry(200, 200),
@@ -23,17 +33,38 @@ const floor = new THREE.Mesh(
 floor.rotation.x = -Math.PI / 2;
 scene.add(floor);
 
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshStandardMaterial({ color: 0x00ff00 })
-);
-cube.position.set(0, 0.5, -2);
-scene.add(cube);
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('../libs/draco/gltf/');
+
+const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
+loader.load('./models/honda_fit.glb', function (gltf) {
+
+    const model = gltf.scene;
+    model.position.set(-.22, -.25, .4);
+    // model.rotation.set(0.1, 0.5, 0);
+    // model.scale.set(30, 30, 30);
+    scene.add(model);
+
+    const box = new THREE.Box3().setFromObject(model);
+    const center = box.getCenter(new THREE.Vector3());
+
+    // animate();
+}, undefined, function (e) {
+    console.error(e);
+});
+
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshStandardMaterial({ color: 0x00ff00 })
+// );
+// cube.position.set(0, 0.5, -2);
+// scene.add(cube);
 
 function animate() {
     renderer.setAnimationLoop(() => {
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        // cube.rotation.x += 0.01;
+        // cube.rotation.y += 0.01;
         renderer.render(scene, camera);
     })
 };
